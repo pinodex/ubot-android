c5c429b75fb077c2d255210b07e6a7f6b82f850c
freaknaruto <ryanfrancis_choresca@yahoo.com>
2018-11-15 11:32:53 +0800

    initial_commit_test

---
+++ app/src/main/java/uhackademia/stinovaliches/com/ubot/login_form.java
@@ -0,0 +1,13 @@
+package uhackademia.stinovaliches.com.ubot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+    }
+}
