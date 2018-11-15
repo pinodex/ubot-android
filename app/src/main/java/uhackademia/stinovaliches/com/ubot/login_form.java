ce1ddd6d905ddaeb7545b6c03216c7bd1525bc60
freaknaruto <ryanfrancis_choresca@yahoo.com>
2018-11-15 23:00:06 +0800

    splashscreen test updated boticon

--- app/src/main/java/uhackademia/stinovaliches/com/ubot/login_form.java
+++ app/src/main/java/uhackademia/stinovaliches/com/ubot/login_form.java
@@ -1,15 +1,20 @@
 package uhackademia.stinovaliches.com.ubot;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class login_form extends AppCompatActivity {
 
     EditText anylinktext;
+    FloatingActionButton floatbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +29,14 @@ public class login_form extends AppCompatActivity {
             browser.setWebViewClient(new WebViewClient());
             browser.loadUrl("http://www.unionbankph.com/");
 
-            Intent intent = new Intent(this, chat_activity.class);
-            startActivity(intent);
+            floatbutton = findViewById(R.id.floatingActionButton);
+            floatbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getApplicationContext(),chat_activity.class);
+                    startActivity(intent);
+                }
+            });
 
     }
 }
