0e0f8f5879008d4ebcab7ca24bb428887cee967f
freaknaruto <ryanfrancis_choresca@yahoo.com>
2018-11-15 13:34:22 +0800

    webview still empty chat_activity empty, will start initial draft

---
+++ app/src/main/java/uhackademia/stinovaliches/com/ubot/chat_activity.java
@@ -0,0 +1,13 @@
+package uhackademia.stinovaliches.com.ubot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chat_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_activity);
+    }
+}
--- app/src/main/java/uhackademia/stinovaliches/com/ubot/login_form.java
+++ app/src/main/java/uhackademia/stinovaliches/com/ubot/login_form.java
@@ -2,12 +2,24 @@ package uhackademia.stinovaliches.com.ubot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class login_form extends AppCompatActivity {
+
+    EditText anylinktext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        //anylinktext = findViewById(R.id.anylinktext);
+
+        //String anylink = anylinktext.getText().toString();
+        WebView browser = findViewById(R.id.webview);
+        browser.setWebViewClient(new WebViewClient());
+        browser.loadUrl("https://www.google.com");
     }
 }
